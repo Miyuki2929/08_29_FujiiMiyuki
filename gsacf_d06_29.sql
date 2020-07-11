be73eba9cd89992f72ad0cf7b624644070a25c77
Miyuki2929 <66865339+Miyuki2929@users.noreply.github.com>
2020-07-11 19:46:01 +0900

    Add files via upload

---
+++ gsacf_d06_29.sql
@@ -0,0 +1,139 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- ホスト: 127.0.0.1
+-- 生成日時: 2020-07-11 12:44:27
+-- サーバのバージョン： 10.4.11-MariaDB
+-- PHP のバージョン: 7.4.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- データベース: `gsacf_d06_29`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `like_table`
+--
+
+CREATE TABLE `like_table` (
+  `id` int(12) NOT NULL,
+  `user_id` int(12) NOT NULL,
+  `todo_id` int(12) NOT NULL,
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- テーブルのデータのダンプ `like_table`
+--
+
+INSERT INTO `like_table` (`id`, `user_id`, `todo_id`, `created_at`) VALUES
+(1, 1, 2, '2020-07-11 17:23:00');
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `todo_table`
+--
+
+CREATE TABLE `todo_table` (
+  `id` int(12) NOT NULL,
+  `todo` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `deadline` date NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- テーブルのデータのダンプ `todo_table`
+--
+
+INSERT INTO `todo_table` (`id`, `todo`, `deadline`, `created_at`, `updated_at`) VALUES
+(2, 'BBQ', '2020-07-11', '2020-06-25 00:15:18', '2020-06-25 00:15:18'),
+(3, 'www', '2020-06-27', '2020-06-27 15:18:48', '2020-06-27 15:18:48'),
+(4, 'tttt', '2020-06-17', '2020-06-27 16:40:35', '2020-06-27 16:40:35'),
+(7, 'ビアガーデン', '2020-07-04', '2020-07-02 22:36:14', '2020-07-02 22:36:14'),
+(9, '課題提出', '2020-07-16', '2020-07-11 15:16:13', '2020-07-11 15:16:13');
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `users_table`
+--
+
+CREATE TABLE `users_table` (
+  `id` int(11) NOT NULL,
+  `username` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `is_admin` int(11) NOT NULL,
+  `is_deleted` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `update_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- テーブルのデータのダンプ `users_table`
+--
+
+INSERT INTO `users_table` (`id`, `username`, `password`, `is_admin`, `is_deleted`, `created_at`, `update_at`) VALUES
+(1, 'testuser', '123456', 0, 0, '2020-07-04 16:05:17', '2020-07-04 16:05:17');
+
+--
+-- ダンプしたテーブルのインデックス
+--
+
+--
+-- テーブルのインデックス `like_table`
+--
+ALTER TABLE `like_table`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- テーブルのインデックス `todo_table`
+--
+ALTER TABLE `todo_table`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- テーブルのインデックス `users_table`
+--
+ALTER TABLE `users_table`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- ダンプしたテーブルのAUTO_INCREMENT
+--
+
+--
+-- テーブルのAUTO_INCREMENT `like_table`
+--
+ALTER TABLE `like_table`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- テーブルのAUTO_INCREMENT `todo_table`
+--
+ALTER TABLE `todo_table`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- テーブルのAUTO_INCREMENT `users_table`
+--
+ALTER TABLE `users_table`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
